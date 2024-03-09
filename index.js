2f3dba5504a3ef6ba704878e05151bdbdd596502
omar2-2khaled <UGS.141015UGS.141015@ci.suez.edu.eg>
2024-03-09 21:57:20 +0200

    first

--- index.js
+++ index.js
@@ -13,24 +13,24 @@ const url = require('url');
 // fs.writeFileSync('./txt/output.txt',textout);
 // console.log(textout);
 // //Non-blocking 
-         // fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
-        //     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
-        //         console.log(data2);
-        //         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
-        //                 coasdsafnsole.log(data3);
-        //                 fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,err=>{
-        //                     console.log("final Done");
-        //                 })
-        //         })
-        //     });
+// fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+//         console.log(data2);
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+//                 coasdsafnsole.log(data3);
+//                 fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,err=>{
+//                     console.log("final Done");
+//                 })
+//         })
+//     });
 
-        // });
-        // console.log('ALL IS WILL');
+// });
+// console.log('ALL IS WILL');
 
 ////////////////
 /////server//////
 /////////////////.
-const replaceTemplate = (temp, product) =>{
+const replaceTemplate = (temp, product) => {
     let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
     output = output.replace(/{%IMAGE%}/g, product.image);
     output = output.replace(/{%QUANTITY%}/g, product.quantity);
@@ -54,28 +54,28 @@ const dataObj = JSON.parse(jsonData);
 const server = http.createServer((req, res) => {
     //const pathname = req.url;
 
-const {query , pathname} = url.parse(req.url,true);
-console.log( url.parse(req.url,true));
-   
-    if (pathname === '/' || pathname === '/overview') { // Fix the condition here
+    const { query, pathname } = url.parse(req.url, true);
+    //console.log( url.parse(req.url,true));
+
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         const cardsHtml = dataObj
-          .map((element) => replaceTemplate(tempCard, element))
-          .join('');
+            .map((element) => replaceTemplate(tempCard, element))
+            .join('');
         const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml);
         res.end(output);
 
-     }
-     //product
-     else if (pathname === '/product') {
+    }
+    //product
+    else if (pathname === '/product') {
         res.writeHead(200, { 'Content-type': 'text/html' });
         const product = dataObj[query.id];
-        const output = replaceTemplate(tempProduct,product);
+        const output = replaceTemplate(tempProduct, product);
         res.end(output);
     }
     // API
     else if (pathname === '/api') {
-        res.writeHead(200, { 'Content-type': 'application/json' }); 
+        res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(jsonData);
     }
     // ERROR
